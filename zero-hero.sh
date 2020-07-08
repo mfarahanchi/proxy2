a92862aa04b663bf346952098a995d606ae21b7f
Mohsen F <mohsenfarahanchi@outlook.com>
2020-07-08 15:13:03 +0430

    Update zero-hero.sh

--- zero-hero.sh
+++ zero-hero.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+sudo rm /var/lib/dpkg/lock
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo dpkg --configure -a
+sudo apt update
 sudo apt install -y python 
 ./setup_https_intercept.sh
 python proxy2.py
